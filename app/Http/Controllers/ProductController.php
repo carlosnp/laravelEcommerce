64a1af3ae6bfd328a60d7f6305f76852f55b9a95
machinery <machinery.e@gmail.com>
2019-08-06 16:11:50 -0400

    edita los productos

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -95,7 +95,14 @@ class ProductController extends Controller
      */
     public function update(Request $request, Product $product)
     {
-        //
+        // return $request->all();
+        // return $product;
+        $request['detail']=$request->description;
+        unset($request['description']);
+        $product->update($request->all());
+        return response([
+            'data' => new ProductResource($product)
+        ], Response::HTTP_CREATED);
     }
 
     /**
