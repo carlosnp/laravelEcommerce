5886bd8312cae0ab6c762266478ea6009aaf02ec
machinery <machinery.e@gmail.com>
2019-08-07 13:01:52 -0400

    cambios en el archivo de excepciones

--- app/Exceptions/ExceptionTrait.php
+++ app/Exceptions/ExceptionTrait.php
@@ -10,16 +10,29 @@ trait ExceptionTrait
 {
     public function apiException($request, $e){
         // Si no encuentra el producto
-        if ($e instanceof ModelNotFoundException) {
-            return response()->json([
-                "errors" => __("Product not found")
-            ], Response::HTTP_NOT_FOUND);
+        if ($this->isModel($e)) {
+            return $this->ModelResponse($e);
         }
         // Ruta incorrecta
-        if ($e instanceof NotFoundHttpException) {
-            return response()->json([
-                "errors" => __("Incorect route")
-            ], Response::HTTP_NOT_FOUND);
+        if ($this->isHttp($e)) {
+            return $this->HttpResponse($e);
         }
+    }
+
+    public function isModel($e) {
+        return $e instanceof ModelNotFoundException;
+    }
+    public function isHttp($e) {
+        return $e instanceof NotFoundHttpException;
+    }
+    public function ModelResponse($e) {
+        return response()->json([
+            "errors" => __("Product not found")
+        ], Response::HTTP_NOT_FOUND);
+    }
+    public function HttpResponse($e) {
+        return response()->json([
+            "errors" => __("Incorect route")
+        ], Response::HTTP_NOT_FOUND);
     }
 }
